e8bde59203a4531d31bf281c892b0155027bbc2b
monst <17f17018@mec.edu.om>
2024-06-08 23:04:14 +0400

    add reaming file

---
+++ app/src/main/java/com/example/teleconnect2/DownloadTaskInfoFragment.java
@@ -0,0 +1,20 @@
+package com.example.teleconnect2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DownloadTaskInfoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_download_task_info, container, false);
+    }
+}
+
---
+++ app/src/main/java/com/example/teleconnect2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.teleconnect2;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Change the status bar color
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+}
